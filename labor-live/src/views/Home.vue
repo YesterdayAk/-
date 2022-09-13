df76f9b40a448335cc9083c931c2d59c91cd1303
Yesterday Once More <50062529+YesterdayAk@users.noreply.github.com>
2022-09-13 16:59:03 +0800

    live

--- labor-live/src/views/Home.vue
+++ labor-live/src/views/Home.vue
@@ -604,6 +604,7 @@ export default defineComponent({
         })
         .then((res: any) => {
           if (res.data.code == 200) {
+            console.log(111);
             store.$patch({
               liveTitle: res.data.result.name,
               liveBrowse: res.data.result.viewPersonTime,
